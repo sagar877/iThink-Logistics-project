18c0a57baf002dcf8a0329645ae0137e7c2f600f
Pritam Rajbhar <pritamrajbhar2001@gmail.com>
2025-06-19 23:41:56 +0530

    changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\ApiUserService;
 use Illuminate\Support\Facades\Hash;
